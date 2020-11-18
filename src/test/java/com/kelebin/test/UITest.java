97d016ec8e8f57f686a270d063be574380f22fd3
kevin andrade <kevinandrade_10@hotmail.com>
2020-11-18 13:18:07 -0300

    Completed Test case run -Added whole webPage navigation with captcha validation

--- src/test/java/com/kelebin/test/UITest.java
+++ src/test/java/com/kelebin/test/UITest.java
@@ -3,6 +3,7 @@ package com.kelebin.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.*;
 import java.io.IOException;
 
 import com.kelebin.pages.HomePage;
@@ -10,6 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,15 +26,16 @@ public class UITest {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://demo.cs-cart.com");
+        driver.manage().window().maximize();
     }
 
     @After
     public void after() throws Exception {
-        // driver.quit();
+         driver.close();
     }
 
     @Test
-    public void test() throws IOException {
+    public void test() throws IOException, InterruptedException {
 
         String productVar = ("T-shirt, Color: Black");
 
@@ -45,26 +48,61 @@ public class UITest {
         WebElement product = driver.findElement(By.name("product_form_280"));
         product.click();
 
+        String prodValue = driver.findElement(By.xpath("//*[@id=\"sec_discounted_price_280\"]")).getText();
+
        // Add this product to the cart;
 
         driver.findElement(By.id("button_cart_280")).click();
 
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div[1]/a")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"sw_dropdown_8\"]/a/span/span[1]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"dropdown_8\"]/div/div[2]/div[1]/a")).click();
+
+
        // Check the item on the cart and total price;
 
-        /*WebElement CartDD = driver.findElement(By.id("sw_dropdown_8"));
-        CartDD.click();*/
-
-        driver.get("https://demo.cs-cart.com/stores/ecc67790cac3b9b0/cart/");
-        //driver.findElement(By.linkText("View cart")).click();
         assertTrue(driver.getPageSource().contains(productVar));
-        //assertEquals(productVar,driver.findElement(By.t));
+
+        String totalPrice = driver.findElement(By.xpath("//*[@id=\"sec_product_subtotal_3924321467\"]")).getText();
+
+        assertEquals(totalPrice,prodValue);
+
 
        // Do the Checkout;
-        driver.findElement(By.linkText("Proceed to checkout")).click();
+        driver.findElement(By.xpath("//*[@id=\"tygh_main_container\"]/div[3]/div/div[2]/div/div/div/div[3]/div[2]/a")).click();
 
 
        // Select the “Phone Order” Payment;
+
+        WebElement inputAddress = driver.findElement(By.id("litecheckout_s_address"));
+        inputAddress.sendKeys("random street");
+
+        WebElement inputEmail = driver.findElement(By.id("litecheckout_email"));
+        inputEmail.sendKeys("randomEmail@gmail.com");
+
+        driver.findElement(By.xpath("//body")).sendKeys(Keys.PAGE_DOWN);
+
+        Thread.sleep(1000);
+        WebElement phoneOrd = driver.findElement(By.xpath("//*[@id=\"payments_2\"]"));
+        phoneOrd.click();
        // Proceed with the order;
+
+        Thread.sleep(1000);
+        WebElement inputPhone = driver.findElement(By.id("customer_phone"));
+        inputPhone.sendKeys("(403) 274-14177");
+
+        WebElement checkBox = driver.findElement(By.name("accept_terms"));
+        checkBox.click();
+
+        WebElement placeOrderBtn = driver.findElement(By.id("litecheckout_place_order"));
+        placeOrderBtn.click();
+
+        //CAPTCHA :D
+        assertTrue(driver.getPageSource().contains("Anti-bot validation"));
     }
 
 }
