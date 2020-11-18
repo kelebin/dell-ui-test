a718dfafc9bcaccfec100ca7409549eef968c4e3
kevin andrade <kevinandrade_10@hotmail.com>
2020-11-18 02:08:56 -0300

    Dell UI Test -First setup commit -Added files to gitignore

---
+++ src/main/java/com/kelebin/App.java
@@ -0,0 +1,13 @@
+package com.kelebin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
