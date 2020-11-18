b3be97bd13bf1a08b5e3d28eaf7a5e00f2195433
kevin andrade <kevinandrade_10@hotmail.com>
2020-11-18 13:49:25 -0300

    Addition of suit class

---
+++ src/test/java/com/kelebin/suit/Suit.java
@@ -0,0 +1,13 @@
+package com.kelebin.suit;
+
+
+import com.kelebin.test.UITest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UITest.class
+})
+public class Suit {
+}
